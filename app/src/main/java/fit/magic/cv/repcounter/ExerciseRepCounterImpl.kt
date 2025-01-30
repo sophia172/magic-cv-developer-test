171794f447ad697cfe34bba06724c9f264f8abd2
Ying Liu <sophia17_2@hotmail.com>
2025-01-30 15:47:11 +0000

    [feature] add python script. Crash might be caused by the emulator webcam connection

--- app/src/main/java/fit/magic/cv/repcounter/ExerciseRepCounterImpl.kt
+++ app/src/main/java/fit/magic/cv/repcounter/ExerciseRepCounterImpl.kt
@@ -2,14 +2,19 @@
 
 package fit.magic.cv.repcounter
 
+import com.chaquo.python.Python
 import fit.magic.cv.PoseLandmarkerHelper
 
 class ExerciseRepCounterImpl : ExerciseRepCounter() {
 
     override fun setResults(resultBundle: PoseLandmarkerHelper.ResultBundle) {
-        // process pose data in resultBundle
-        //
-        // use functions in base class incrementRepCount(), sendProgressUpdate(),
-        // and sendFeedbackMessage() to update the UI
+        val python = Python.getInstance()
+        val pyObject = python.getModule("script")  // Name of your Python script without .py
+        val result = pyObject.callAttr("greet", "World")  // Call the Python function
+//        println(result.toString())
+        incrementRepCount()
+        val currentReps = 0.1f
+        sendProgressUpdate(currentReps)
+        sendFeedbackMessage(result.toString())
     }
 }
